cd18c92c3e0192c18fba4119d2c2deac774bf36e
jlhdz17 <92648078+jlhdz17@users.noreply.github.com>
2022-01-07 22:02:28 -0800

    Initial 2

--- student.js
+++ student.js
@@ -2,3 +2,14 @@ function confirmlink684(linkname) {
     if (confirm("Are you sure you want to jump to " + linkname + "?")) 
          location = linkname; 
  }
+
+
+
+
+ var myname = "Jorge Hernandez"; 
+ var message2 = "cs190 Javascript Lab"; 
+ var code = "434"
+ 
+ function displayinfo257 ( ) {  
+        alert ( "My name is " + myname +  "\n I am currently attempting the " + message2 + "\nOh now whats happening..." + code ); 
+ } 
