b5362823bee151e42f8d2881fccfccd0048e76d6
jlhdz17 <92648078+jlhdz17@users.noreply.github.com>
2022-01-11 12:34:08 -0800

    Lab 3 Commit Pt 2

--- student.js
+++ student.js
@@ -13,3 +13,26 @@ function confirmlink684(linkname) {
  function displayinfo257 ( ) {  
         alert ( "My name is " + myname +  "\n I am currently attempting the " + message2 + "\nOh now whats happening..." + code ); 
  } 
+
+
+
+// declare this variable outside of your functions
+currnum = 1;     // selects which item that is "on special"
+
+function displayspecial410() {
+
+   if (currnum == 1) {
+	  alert("The current special is #1");
+   }
+   if (currnum == 2)  {
+	  alert("The current special is #2");
+   }
+   if (currnum == 3 )  {
+	  alert("The current special is #3");
+   }
+   currnum = currnum + 1;
+   if (currnum > 3) {
+	 currnum = 1 ;  // wrap around
+   }
+
+}
